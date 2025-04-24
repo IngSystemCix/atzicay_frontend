b31150faf08fdd98e9ad1fd7059657fcd6868d0e
ramos marrufo daniel <marrufodanieldavid0109@gmail.com>
2025-04-24 02:14:46 -0500

    feat: Google data capture

--- src/app/presentation/shared/header/header.component.ts
+++ src/app/presentation/shared/header/header.component.ts
@@ -1,11 +1,24 @@
 // src/app/presentation/shared/header/header.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  userName = 'Franklin T.';
+export class HeaderComponent implements OnInit {
+  userName: String = '';
+  picture: String = '';
+
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe((user) => {
+      if (user) {
+        this.userName = user.name || '';
+        this.picture = user.picture || '';
+      }
+    });
+  }
 }
