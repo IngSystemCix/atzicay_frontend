c6db4d37732316a028a9990232677b75000a72ac
R0dri-Dev <U22232910@utp.edu.pe>
2025-06-04 12:51:04 -0500

    fix gameSetting problem

--- src/app/core/domain/interface/game-setting.ts
+++ src/app/core/domain/interface/game-setting.ts
@@ -1,11 +1,4 @@
 export interface GameSetting {
-    id: number;
-    idGameInstance: number;
-    ProgrammerId: number;
-    Name: string;
-    Activated: string;
-    StarTime: Date;
-    EndTime: Date;
-    Attems: number;
-    MaximumTime: number;
+    ConfigKey: string;
+    ConfigValue: string;
 }
