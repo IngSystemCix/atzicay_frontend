23101195e5a5fa428ef1649a6913678205a23156
IngSystemCix <juanromerocollazos@gmail.com>
2025-06-04 01:27:37 -0500

    feat: branch correction

---
+++ src/app/presentation/features/config-game/config-game.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-config-game',
+  imports: [],
+  templateUrl: './config-game.component.html',
+  styleUrl: './config-game.component.css'
+})
+export class ConfigGameComponent {
+
+}
